7e53ba66204c9e88c0d9e402c2cbd0cc2b6668cb
Daniël Kroese <d@nielkroese.nl>
2024-01-21 17:44:03 +0100

    Initial commit with Xcode project

---
+++ MetalShading/MetalShading/User Interface/MetalShadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalShadingApp.swift
+//  MetalShading
+//
+//  Created by Daniël Kroese on 21/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MetalShadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
