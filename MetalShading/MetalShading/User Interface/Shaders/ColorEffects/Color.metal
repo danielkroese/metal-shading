4db89ff6ca5b2a833ed46941855190fb0e2839d0
Daniël Kroese <d@nielkroese.nl>
2024-01-22 21:38:26 +0100

    Use visualEffect to pass geometry to shader

--- MetalShading/MetalShading/User Interface/Shaders/ColorEffects/Color.metal
+++ MetalShading/MetalShading/User Interface/Shaders/ColorEffects/Color.metal
@@ -3,5 +3,5 @@ using namespace metal;
 
 [[ stitchable ]] 
 half4 color(float2 position, half4 color) {
-    return half4(position.x/255.0, 50.0, 50.0, 1.0);
+    return half4(position.x / 255.0, position.y / 255.0, 0.0 / 255.0, 1.0);
 }
--- MetalShading/MetalShading/User Interface/Shaders/ColorEffects/View+ColorEffect.swift
+++ MetalShading/MetalShading/User Interface/Shaders/ColorEffects/View+ColorEffect.swift
@@ -1,14 +1,22 @@
 import SwiftUI
 
-extension View {
-    func colorShader() -> some View {
-        modifier(ColorShader())
+struct ColorShader: ViewModifier {
+    let isEnabled: Bool
+    
+    func body(content: Content) -> some View {
+        content
+            .visualEffect { content, geometry in
+                content.colorEffect(shader(of: geometry.size), isEnabled: isEnabled)
+            }
+    }
+    
+    private func shader(of contentSize: CGSize) -> Shader {
+        ShaderLibrary.sizeAwareColor(.float2(contentSize))
     }
 }
 
-struct ColorShader: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .colorEffect(ShaderLibrary.color())
+extension View {
+    func colorShader(isEnabled: Bool = true) -> some View {
+        modifier(ColorShader(isEnabled: isEnabled))
     }
 }
